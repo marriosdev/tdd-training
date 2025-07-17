825a6bee4fa2630dd54fe2bc440284373357c619
marriosdev <barrigacraft@hotmail.com>
2025-07-16 21:50:58 -0300

    test: new tests

--- app/Services/ViaCepApiService.php
+++ app/Services/ViaCepApiService.php
@@ -1,18 +1,32 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Services;
 
 use App\Dtos\Cep\CepDto;
+use RuntimeException;
+use stdClass;
 
 class ViaCepApiService
 {
+    /**
+     * 
+     * @param string $cep
+     * @return CepDto
+     */
     public function getInfos(string $cep)
     {
-        return new CepDto((object) $this->getCepFromApi($cep));
+        $viaCepResponse = $this->getCepFromApi($cep);
+
+        if($viaCepResponse->statusCode !== 200) {
+            throw new RuntimeException("Ocorreu um erro ao buscar o CEP");
+        }
+
+        return new CepDto((object) $viaCepResponse->data);
     }
 
-    public function getCepFromApi(string $cep)
+    public function getCepFromApi(string $cep): stdClass
     {
-
+        return new stdClass();
     }
 }
--- tests/Unit/ViaCepApiServiceTest.php
+++ tests/Unit/ViaCepApiServiceTest.php
@@ -4,14 +4,17 @@ namespace Tests\Unit;
 
 use App\Dtos\Cep\CepDto;
 use App\Services\ViaCepApiService;
+use RuntimeException;
+use stdClass;
 use Tests\TestCase;
 
 class ViaCepApiServiceTest extends TestCase
 {
     public function test_should_return_cep_dto_if_cep_valid(): void
     {
-        // A
+        // Arange
         $validCep = "01001000";
+        
         $expectedArray = [
             'cep' => '01001-000',
             'logradouro' => 'Praça da Sé',
@@ -27,20 +30,50 @@ class ViaCepApiServiceTest extends TestCase
             'ddd' => '11',
             'siafi' => '7107',
         ];
+        
+        $viaCepResponse = new stdClass();
+        $viaCepResponse->statusCode = 200;
+        $viaCepResponse->data = $expectedArray;
 
         $dtoExpected = new CepDto((object) $expectedArray);
-
+        
         $serviceMock = $this->getMockBuilder(ViaCepApiService::class)
             ->onlyMethods(['getCepFromApi'])
             ->getMock();
-    
+
         $serviceMock->expects($this->once())
-            ->method('getCepFromApi')
-            ->with($validCep)
-            ->willReturn($expectedArray);
+            ->method(constraint: 'getCepFromApi')
+            ->with(arguments: $validCep)
+            ->willReturn($viaCepResponse);
     
+        // Action
         $dtoReturned = $serviceMock->getInfos($validCep);
     
+        // Assert
         $this->assertEquals($dtoExpected->toArray(), $dtoReturned->toArray());
+    }
+
+    public function test_should_throw_argument_exception_if_cep_invalid(): void
+    {
+        // Arange
+        $invalidCep = "2319387123";
+       
+        $viaCepApiResponse = new stdClass();
+        $viaCepApiResponse->statusCode = 404;
+        
+        $serviceMock = $this->getMockBuilder(ViaCepApiService::class)
+            ->onlyMethods(['getCepFromApi'])
+            ->getMock();
+
+        $serviceMock->expects($this->once())
+            ->method('getCepFromApi')
+            ->with($invalidCep)
+            ->willReturn($viaCepApiResponse);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Ocorreu um erro ao buscar o CEP");
+
+        // Assert
+        $serviceMock->getInfos($invalidCep);
     }
 }
